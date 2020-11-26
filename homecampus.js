1a6d92265c300acbdf8d9018109f4045f16aa0dc
kholajamshad <57991557+kholajamshad@users.noreply.github.com>
2020-11-26 17:23:56 +0500

    added video playability

--- homecampus.js
+++ homecampus.js
@@ -1,8 +1,30 @@
 let homeicon=document.getElementById("homeicon");
 let campusicon=document.getElementById("campusicon");
-/*
-homeicon.addEventListener('click', () => {
-	homevideo.requestFullscreen(), {once:true}
-	});
+let video=document.getElementById("vid");
+let close=document.getElementById("close");
 
-*/
+homeicon.addEventListener('click', () => {
+	video.src="Khola_whyedit.mp4";
+	video.style.display="block";
+	close.style.display="block";
+	video.play();
+	homeicon.style.display="none";
+	campusicon.style.display="none";
+});
+
+campusicon.addEventListener('click', () => {
+	video.src="Khola_whyedit.mp4";
+	video.style.display="block";
+	close.style.display="block";
+	video.play();
+	homeicon.style.display="none";
+	campusicon.style.display="none";
+});
+
+close.addEventListener('click', () => {
+	video.pause();
+	close.style.display="none";
+	video.style.display="none";
+	homeicon.style.display="block";
+	campusicon.style.display="block";
+})
